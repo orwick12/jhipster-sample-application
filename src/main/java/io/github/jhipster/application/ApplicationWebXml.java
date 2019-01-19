550f3326d03a1261d3b4cfdaacc48732039fd49c
Brian Orwick <Orwick12@outlook.com>
2019-01-18 23:14:48 -0500

    Starting project

--- src/main/java/io/github/jhipster/application/ApplicationWebXml.java
+++ src/main/java/io/github/jhipster/application/ApplicationWebXml.java
@@ -5,6 +5,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
+ * Testing
  * This is a helper Java class that provides an alternative to creating a web.xml.
  * This will be invoked only when the application is deployed to a Servlet container like Tomcat, JBoss etc.
  */
